3853de39622b5da4bcbf1bc1b6776766b76a867d
An <haantran96@gmail.com>
2020-04-07 09:04:00 +0300

    adding onSubmit, onChange functions

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,22 +2,56 @@ import React, {Component} from 'react';
 
 class TaskForm extends Component {
 
+    constructor(props){
+        super (props);
+        this.state = {
+            name : '',
+            status : false
+        }
+    }
+
+   onCloseForm = () => {
+       this.props.onCloseForm();
+   }
+
+   onChange = (event) => {
+       var target = event.target;
+       var name = target.name;
+       var value = target.value;
+       if (name === "status") {
+           value = target.value === 'true' ? true : false;
+       }
+       this.setState({
+           [name] : value
+       });
+   }
+
+   onSubmit = (event) => {
+       event.preventDefault();
+       this.props.onSubmit(this.state);
+   }
   render() {
     return (
         <div className="panel panel-warning">
             <div className="panel-heading">
-                <h3 className="panel-title">Add Task</h3>
+                <h3 className="panel-title">Add Task
+                    <span 
+                        className= "fa fa-times-circle text-right"
+                        onClick={this.onCloseForm}>
+
+                    </span>
+                </h3>
             </div>
             <div className="panel-body">
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label>Task Name :</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name = "name" value = {this.state.name} onChange={this.onChange}/>
                     </div>
                     <label>Status :</label>
-                    <select className="form-control" required="required">
-                        <option value="1">Finished</option>
-                        <option value="0">Unfinished</option>
+                    <select className="form-control" required="required" name="status" value={this.state.status} onChange={this.onChange}>
+                        <option value={true}>Finished</option>
+                        <option value={false}>Unfinished</option>
                     </select>
                     <br/>
                     <div className="text-center">
