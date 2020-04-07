100e34ec29077480551a3c9b5e319019bba9013a
An <haantran96@gmail.com>
2020-04-07 22:44:30 +0300

    adding onSort function to sort alphabetically and by status

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,47 +1,48 @@
 import React, {Component} from 'react';
 class Sort extends Component {
+    
+    onClick = (sortBy, sortValue) => {
+        this.props.onSort(sortBy, sortValue);
+    }
+    render() {
+        return (
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <div className="dropdown">
+                    <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        Filter <span className="fa fa-caret-square-o-down ml-5"></span>
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <li onClick={() => this.onClick('name',1)}>
+                            <a role="button" 
+                                className= {(this.props.sortBy === 'name' && this.props.sortValue===1) ? 'sort_selected':''} >
+                                <span className="fa fa-sort-alpha-asc pr-5">
+                                    Name A-Z
+                                </span>
+                            </a>
+                        </li>
+                        <li onClick={() => this.onClick('name',-1)}>
+                            <a role="button"
+                                className= {(this.props.sortBy === 'name' && this.props.sortValue===-1) ? 'sort_selected':''} >
+                                <span className="fa fa-sort-alpha-desc pr-5">
+                                    Name Z-A
+                                </span>
+                            </a>
+                        </li>
+                        <li role="separator" className="divider"></li>
+                        <li onClick={() => this.onClick('status',1)}>
+                            <a role="button" 
+                                className= {(this.props.sortBy === 'status' && this.props.sortValue===1) ? 'sort_selected':''} >
 
-  render() {
-    return (
-        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <div className="dropdown">
-                <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    Filter <span className="fa fa-caret-square-o-down ml-5"></span>
-                </button>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <li>
-                        <a role="button">
-                                    <span className="fa fa-sort-alpha-asc pr-5">
-                                        
-                                        <nav className="navbar navbar-default navbar-fixed-bottom" role="navigation">
-                                        <a className="navbar-brand" href="#">Title</a>
-                                        <ul className="nav navbar-nav">
-                                            <li className="active">
-                                            <a href="#">Home</a>
-                                            </li>
-                                            <li>
-                                            <a href="#">Link</a>
-                                            </li>
-                                        </ul>
-                                        </nav>
-                                        A-Z
-                                    </span>
-                                </a>
-                    </li>
-                    <li>
-                        <a role="button">
-                                    <span className="fa fa-sort-alpha-desc pr-5">
-                                        Name Z-A
-                                    </span>
-                                </a>
-                    </li>
-                    <li role="separator" className="divider"></li>
-                    <li><a role="button">Finished</a></li>
-                    <li><a role="button">Unfinished</a></li>
-                </ul>
+                                Finished</a></li>
+                        <li onClick={() => this.onClick('status',-1)}>
+                            <a role="button" 
+                            className= {(this.props.sortBy === 'status' && this.props.sortValue===-1) ? 'sort_selected':''} >
+
+                                Unfinished</a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    );
+        );
     }
 }
 
